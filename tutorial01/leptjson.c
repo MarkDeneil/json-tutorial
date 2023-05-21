8ce3f284e162f1cbecbbb7c89605a2645fe82b38
MarkDeneil <1102178708@qq.com>
2023-05-21 14:20:38 +0800

    tutorial-1

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -34,7 +34,7 @@ static int lept_parse_true(lept_context * c, lept_value * v) {
     return LEPT_PARSE_OK;
 }
 
-lept_parse_false(lept_context * c, lept_value * v) {
+static int lept_parse_false(lept_context * c, lept_value * v) {
     EXPECT(c, 'f');
     if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
         return LEPT_PARSE_INVALID_VALUE;
